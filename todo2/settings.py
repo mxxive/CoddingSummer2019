4f6c5a054d696e5d87676cb27edf9b2ff48ed31b
ilimyo <lilimiao.hu@gmail.com>
2019-05-20 01:23:22 +0900

    Host binding ip changed

--- todo2/settings.py
+++ todo2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*lcf7ju_kqxb$5hft@!-6&$z8pj&v7e%b6sm8ynjbc8yd6rv0!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
